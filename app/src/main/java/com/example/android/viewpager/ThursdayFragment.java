1ed19ba8afeee9234cdcf8585d3b7094b3a5bda2
alejo9226 <alejo9226@gmail.com>
2017-06-26 23:17:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/viewpager/ThursdayFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alejandroalfaro on 5/12/16.
+ */
+
+public class ThursdayFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_thursday, container, false);
+    }
+
+
+}
